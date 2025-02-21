605cc34f62612a002af467a3149a5ea4fa9df376
Ajax <rajdeeptrade17@gmail.com>
2025-02-21 13:26:58 +0530

    Added users on dev branch

---
+++ main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+
+app = FastAPI()  # Create a FastAPI instance
+
+@app.get("/")  # Defines an API route at /. (GET request)
+def read_root():  #  A function that runs when a request is made to /.
+    return {"message": "Hello, FastAPI!"}  # Return JSON response
+
+
+
+@app.get("/users/")
+def get_users():
+    return [{"name": "Alice"}, {"name": "Bob"}]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
